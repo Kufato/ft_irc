7bab5c6b7c57247ac9ff204374335e215b98a5fc
Augustin Joliet <ajoliet@2A2.42angouleme.fr>
2024-02-27 20:02:01 +0100

    juste parce que je me faisais chier les args c'est bon

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: axcallet <axcallet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 14:04:34 by axcallet          #+#    #+#             */
-/*   Updated: 2024/02/27 14:10:45 by axcallet         ###   ########.fr       */
+/*   Updated: 2024/02/27 19:49:56 by ajoliet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 int main (int argc, char **argv) {
 	if (argc != 3) {
 		std::cerr << "Wrong number of arguments" << std::endl;
+		return (1);
+	}
+	std::string tmp = argv[1];
+	if (tmp.find_first_not_of("0123456789") != std::string::npos)
+	{
+		std::cout << "the port must have only numeric characters" << std::endl;
 		return (1);
 	}
 	try {
